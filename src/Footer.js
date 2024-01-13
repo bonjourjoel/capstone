9eefba79b22b81b76c9e671c291ba3e3e8120b44
joel <bonjourjoel@gmail.com>
2024-01-13 12:43:16 +0100

    add css on header and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,38 +1,29 @@
-import logo from "./assets/Logo.svg";
-
+import pic from "./assets/restauranfood.jpg";
+import './Footer.css';
 
 export default function Footer() {
   return (
     <footer>
       <div>
-        <img src={logo} alt='Little lemon logo'/>
+        <img src={pic} className="footer-pic" alt='Little lemon food'/>
       </div>
       <div>
-        <p>Doormat Navigation</p>
-        <ul>
-          <li><a href='home'>Home</a></li>
-          <li><a href='about'>About</a></li>
-          <li><a href='menu'>menu</a></li>
-          <li><a href='reservations'>Reservations</a></li>
-          <li><a href='order-online'>Order Online</a></li>
-          <li><a href='login'>Login</a></li>
-        </ul>
+          <div><a href='home'>Home</a></div>
+          <div><a href='about'>About</a></div>
+          <div><a href='menu'>menu</a></div>
+          <div><a href='reservations'>Reservations</a></div>
+          <div><a href='order-online'>Order Online</a></div>
+          <div><a href='login'>Login</a></div>
       </div>
       <div>
-        <p>Contact</p>
-        <ul>
-          <li><a href='contact-address'>Address</a></li>
-          <li><a href='contact-phone-number'>Phone number</a></li>
-          <li><a href='contact-email'>email</a></li>
-        </ul>
+          <div><a href='contact-address'>Address</a></div>
+          <div><a href='contact-phone-number'>Phone number</a></div>
+          <div><a href='contact-email'>email</a></div>
       </div>
       <div>
-        <p>Social media links</p>
-        <ul>
-          <li><a href='https://facebook.com'>Facebook</a></li>
-          <li><a href='https://twitter.com'>Twitter</a></li>
-          <li><a href='https://linkedin.com'>Linkedin</a></li>
-        </ul>
+          <div><a href='https://facebook.com'>Facebook</a></div>
+          <div><a href='https://twitter.com'>Twitter</a></div>
+          <div><a href='https://divnkedin.com'>divnkedin</a></div>
       </div>
     </footer>
   )
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,20 @@
 import logo from "./assets/Logo.svg";
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt='Little lemon logo'/>
+      <nav>
+        <ul>
+          <li><a href='home'>Home</a></li>
+          <li><a href='about'>About</a></li>
+          <li><a href='menu'>Menu</a></li>
+          <li><a href='reservations'>Reservations</a></li>
+          <li><a href='order-online'>Order online</a></li>
+          <li><a href='login'>Login</a></li>
+        </ul>
+      </nav>
     </header>
   )
 }
